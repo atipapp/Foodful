a17e9e2d77eb2458ac5e18353dfe72af6e52a1dd
Attila Papp <papp.attila971@gmail.com>
2018-09-17 19:15:36 +0200

    Add dinner-service

---
+++ backend/dinner-service/src/main/java/io/foodful/dinnerservice/DinnerServiceApplication.java
@@ -0,0 +1,12 @@
+package io.foodful.dinnerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinnerServiceApplication.class, args);
+	}
+}
---
+++ backend/dinner-service/src/test/java/io/foodful/dinnerservice/DinnerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.foodful.dinnerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DinnerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
