89f43a306465c27750af12cdaf5f50ceab82c7ab
Attila Papp <papp.attila971@gmail.com>
2018-09-24 19:03:06 +0200

    Implement jwt based security in commons

---
+++ backend/commons/commons-core/src/main/java/io/foodful/configuration/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package io.foodful.configuration;
+
+import io.foodful.interceptors.UserSecurityFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${foodful.auth.user-data-header}")
+    private String userDataHeaderName;
+
+    @Value("${foodful.auth.jwt.key}")
+    private String jwtKey;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new UserSecurityFilter(userDataHeaderName, jwtKey),
+                BasicAuthenticationFilter.class);
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .csrf().disable();
+    }
+
+}
