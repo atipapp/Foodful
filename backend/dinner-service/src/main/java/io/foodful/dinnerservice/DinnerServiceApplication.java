5d7de9f7c8e0fc1312df957285fef644124bbe57
Attila Papp <papp.attila971@gmail.com>
2018-09-24 19:38:04 +0200

    Set up security in dinner-service

--- backend/dinner-service/src/main/java/io/foodful/dinnerservice/DinnerServiceApplication.java
+++ backend/dinner-service/src/main/java/io/foodful/dinnerservice/DinnerServiceApplication.java
@@ -2,8 +2,12 @@ package io.foodful.dinnerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "io.foodful")
+@ComponentScan({"io.foodful"})
 public class DinnerServiceApplication {
 
 	public static void main(String[] args) {
