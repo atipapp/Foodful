bd46a9e5f988cc7d8ddb8351ed5e85be57063c24
Attila Papp <papp.attila971@gmail.com>
2018-10-07 13:39:57 +0200

    Add auth client

---
+++ backend/auth/auth-api/src/main/java/io/foodful/auth/api/AuthClient.java
@@ -0,0 +1,19 @@
+package io.foodful.auth.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "auth-service-v1", url = "${foodful.clients.prefix}auth-service-v1${foodful.clients.postfix}")
+public interface AuthClient {
+
+    @PostMapping("/oauth/login/social")
+    TokenResponse loginWithSocialProvider(@RequestBody LoginRequest request);
+
+    @PostMapping("/oauth/token")
+    TokenResponse renewAccessToken(@RequestBody AccessTokenRenewalRequest request);
+
+    @PostMapping("/oauth/token/validate")
+    TokenValidationResponse validateAccessToken(@RequestBody TokenValidationRequest request);
+
+}
