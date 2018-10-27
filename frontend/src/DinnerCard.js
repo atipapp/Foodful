78bbae4a369787e1e29927a09633cf273c5f3ec0
Attila Papp <papp.attila971@gmail.com>
2018-10-27 15:20:39 +0200

    Add DinnerCard

---
+++ frontend/src/DinnerCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class DinnerCard extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Mock Dinner</h2>
+        <p>This is where your invitations will appear.</p>
+      </div>
+    );
+  }
+}
+ 
+export default DinnerCard;
--- frontend/src/Dinners.js
+++ frontend/src/Dinners.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
- 
+import DinnerCard from "./DinnerCard";
+
 class Dinners extends Component {
   render() {
     return (
       <div>
-        <h2>Hi,</h2>
-        <p>This is where your invitations will appear.</p>
+        <h2>Your upcoming dinners</h2>
+        {this.renderDinnerCard()}
+
       </div>
     );
   }
+
+  renderDinnerCard() {
+    return (
+      <DinnerCard />
+    );
+  }
 }
- 
+
 export default Dinners;
