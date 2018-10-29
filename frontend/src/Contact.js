61a7ff66494c890bfcf691ab0fd79c54e38bc820
Attila Papp <papp.attila971@gmail.com>
2018-10-29 15:32:17 +0100

    Add api status label to contact page

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -1,6 +1,35 @@
 import React, { Component } from "react";
- 
+
 class Contact extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      health: "Down"
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://staging.foodful.io/api/actuator/health")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            health: result.status
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
     return (
       <div>
@@ -8,9 +37,10 @@ class Contact extends Component {
         <p>The easiest thing to do is send and email
           to the <a href="mailto:papp.attila971@gmail.com">developer</a>.
         </p>
+        <h2>Api status: {this.state.health}</h2>
       </div>
     );
   }
 }
- 
+
 export default Contact;
