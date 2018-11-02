88f9608dbb5544f76414ec07633fbb089e725476
Attila Papp <papp.attila971@gmail.com>
2018-11-02 17:10:13 +0100

    Remove queryparam from logincode

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -5,10 +5,24 @@ class Login extends Component {
         super(props);
         console.log(props);
         this.state = {
-            facebookCode: props.location.search
+            facebookCode: props.location.search.replace("?code=", "")
         }
     }
 
+    loginWithApi(){
+        fetch('https://mywebsite.com/endpoint/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                redirect_uri: 'http://localhost:3000/login',
+                code: this.state.facebookCode,
+                provider: 'FACEBOOK'
+            })
+        })
+    }
 
     render() {
         return (
